01b19705223855768089e4f2c4fa479968b296cb
hiromitsu.ujikawa <hiromitsu.ujikawa@fuller.co.jp>
2026-02-09 21:07:23 +0900

    Add Kotlin program to print bordered string
    Create a simple Kotlin program that reads a string and prints a bordered output.

---
+++ 20260209_2.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+  val s = readLine()!!
+  val l = s.length.toInt()
+  
+  repeat(l+2){
+      print("+")
+  }
+      println("")
+      println("+" + s + "+")
+      
+  repeat(l+2){
+      print("+")
+  }
+
+    
+}
